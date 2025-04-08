5e6abd3775ffad76581be948110d382f34c68bc4
Nicolasdebian <nicolaseduardo224@gmail.com>
2025-04-08 01:38:32 -0300

    Código alterado

--- banco.js
+++ banco.js
@@ -43,7 +43,19 @@ async function buscarUsuario(usuario)
 async function registrarUsuario(usuario)
 {
     const conexao = await conectarBD();
-    const sql = "insert into usuarios(usunome, usuemail, usunascimento, ususenha) values (?,?,?,?)"
+    const sql = "insert into usuario(usunome, usuemail, usunascimento, ususenha) values (?,?,?,?)"
     await conexao.query(sql, [usuario.nome, usuario.email, usuario.nasc, usuario.senha]);
 }
-    module.exports = {buscarUsuario, registrarUsuario};
+
+async function buscarAdmin(admin){
+    const conexao = await conectarBD();
+    const sql = "select * from admin where admemail=? and admsenha=?";
+    const[adminEncontrado] = await conexao.query(sql,[admin.email, admin.senha]);
+    if (adminEncontrado && adminEncontrado.length > 0){
+        return adminEncontrado[0];
+    }else{
+        return{};
+    }
+
+}
+    module.exports = {buscarUsuario, registrarUsuario, buscarAdmin};
